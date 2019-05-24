c2705afde43a8ed10d33f2eb958e3d509bbbba6c
161250142 <1663451070@qq.com>
2019-05-24 13:38:32 +0800

    打包方式/启动方式3

--- src/main/java/com/MycoursesApplication.java
+++ src/main/java/com/MycoursesApplication.java
@@ -3,15 +3,21 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.repository")
 @EntityScan(basePackages = "com.entity")
-public class MycoursesApplication{
+public class MycoursesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MycoursesApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MycoursesApplication.class);
     }
 
 }
